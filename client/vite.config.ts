240964dca332ca4e532a6f72c5d0c54bdfced13d
Peter Ayenoto <ayenotopeter@hotmail.com>
2026-09-14 20:33:42 +0100

    OnRender

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -68,6 +68,6 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: { '/api': { target: 'http://localhost:4000', changeOrigin: true } },
+    proxy: { '/api': { target: 'https://calidad-tthd.onrender.com', changeOrigin: true } },
   },
 })
